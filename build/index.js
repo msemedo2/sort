7bd1e2ae92158e10ef1b692cbccf038b7c3550c3
unknown <miguel.semedo.2@outlook.com>
2022-12-06 11:39:53 +0000

    allow sort on strings

--- build/index.js
+++ build/index.js
@@ -1,8 +1,13 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const Sorter_1 = require("./Sorter");
-const NumberCollection_1 = require("./NumberCollection");
-const numbersCollection = new NumberCollection_1.NumbersCollection([1000, 10, 3, -5, 0]);
-const sorter = new Sorter_1.Sorter(numbersCollection);
+const CharactersCollection_1 = require("./CharactersCollection");
+// const numbersCollection = new NumbersCollection([1000, 10, 3, -5, 0]);
+// const sorter = new Sorter(numbersCollection);
+// sorter.sort();
+// console.log(numbersCollection.data);
+const charactersCollection = new CharactersCollection_1.CharactersCollection('Xaayb');
+const sorter = new Sorter_1.Sorter(charactersCollection);
 sorter.sort();
-console.log(numbersCollection.data);
+console.log(charactersCollection);
+//allow sort on strings
